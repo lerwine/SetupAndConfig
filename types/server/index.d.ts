ab506951400c10d263cd5e5feb9096c496ae3edb
erwinel <leonard.t.erwine.ctr@mail.mil>
2022-09-09 14:48:16 -0400

    Move files to the site_17_mmservices repository

--- types/server/index.d.ts
+++ types/server/index.d.ts
@@ -1,2 +1,1 @@
 /// <reference path="sn_typings_server_scoped/dist/index.d.ts" />
-/// <reference path="x_44813_mmservices/index.d.ts" />
--- types/server/x_44813_mmservices/index.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="../sn_typings_server_scoped/dist/index.d.ts" />
-/// <reference path="api/Site17MMServicesUtil.d.ts" />
